93f51ed1b90f4ced8835022d48ceae47a04edaa4
The Pffy Authors <50888935+pffy@users.noreply.github.com>
2023-05-15 17:07:46 -0700

    Create msg.php

---
+++ msg.php
@@ -0,0 +1,36 @@
+<?php
+
+## name     : msg.php
+## purpose  : type-aware CLI output
+## license  : MIT
+## git      : https://github.com/pffy/msg-php
+
+# msg returns output for debugging
+
+function msg($var) {
+
+  switch(gettype($var)) {
+    case 'string':
+      echo $var;
+      break;
+    case 'float':
+      echo (float)$var;
+      break;
+    case 'integer':
+      echo (int)$var;
+      break;
+    case 'boolean':
+      $var = (string) $var;
+      echo $var;
+      break;
+    case 'object':
+      var_dump($var);
+      break;
+    default:
+      #derp, whatever
+      var_dump($var);
+      break;
+  }
+
+  echo PHP_EOL;
+}
